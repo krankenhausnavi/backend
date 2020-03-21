61024d2b2635bbf710ad085326f7d51bb95c90c5
Philipp Donabauer <philipp.donabauer@tarienna.eu>
2020-03-21 15:51:32 +0100

    Added bad request on when query parameters are not set

--- src/get-institutions.ts
+++ src/get-institutions.ts
@@ -7,8 +7,8 @@ const RANDOM_DATA_COUNT: number = 40;
 export const handler: APIGatewayProxyHandler = async (event, _context) => {
     if (
         !event.queryStringParameters['lon']
-        || event.queryStringParameters['lat']
-        || event.queryStringParameters['area']
+        || !event.queryStringParameters['lat']
+        || !event.queryStringParameters['area']
     ) {
         return {
             statusCode: 400,
