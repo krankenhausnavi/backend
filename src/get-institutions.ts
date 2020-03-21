4f4876f2cfeff861e5bc0558ae0223973d90e2a1
Philipp Donabauer <philipp.donabauer@tarienna.eu>
2020-03-21 17:38:10 +0100

    Added get for single institution and changed paths

--- src/get-institutions.ts
+++ src/get-institutions.ts
@@ -1,16 +1,11 @@
 import {APIGatewayProxyHandler} from 'aws-lambda';
-import randomLocation from 'random-location'
 import 'source-map-support/register';
-
-const RANDOM_DATA_COUNT: number = 40;
 
 export const handler: APIGatewayProxyHandler = async (event, _context) => {
     if (
         !event
-        || !event.queryStringParameters
-        || !event.queryStringParameters['lon']
-        || !event.queryStringParameters['lat']
-        || !event.queryStringParameters['area']
+        || !event.pathParameters
+        || !event.pathParameters['institutionId']
     ) {
         return {
             statusCode: 400,
@@ -18,34 +13,23 @@ export const handler: APIGatewayProxyHandler = async (event, _context) => {
         };
     }
 
-    const longitude: number = parseFloat(event.queryStringParameters['lon']);
-    const latitude: number = parseFloat(event.queryStringParameters['lat']);
-    const area: number = parseFloat(event.queryStringParameters['area']);
+    const institutionId = event.pathParameters['institutionId'];
 
-    const testDataStartPoint = {
-        longitude,
-        latitude
-    };
-
-    const randomData = [];
-
-    for (let i = 0; i < RANDOM_DATA_COUNT; i++) {
-        const randomPoint = randomLocation.randomCirclePoint(testDataStartPoint, area * 1000);
-        const isHospital = i % 2 == 0;
-
-        randomData.push({
-            "id": i,
-            "name": (isHospital ? "Krankenhaus" : "Arztpraxis") + " " + i,
-            "type": isHospital ? "HOSPITAL" : "DOCTOR",
+    return {
+        statusCode: 200,
+        body: JSON.stringify({
+            "id": institutionId,
+            "name": "Krankenhaus",
+            "type": "HOSPITAL",
             "street": "ANYTHING ELSE",
             "city": "ANY_CITY",
             "postal_code": "POSTAL_CODE",
             "phone_number": "123123131",
             "website": "http://example.com",
-            "longitude": randomPoint.longitude,
-            "latitude": randomPoint.latitude,
+            "longitude": 124341.31244,
+            "latitude": 124341.31244,
             "comment": "Bitte nicht ohne Termin",
-            "opening_hours":[
+            "opening_hours": [
                 {
                     "day": "MONTAG",
                     "start_time": "08:00",
@@ -58,33 +42,20 @@ export const handler: APIGatewayProxyHandler = async (event, _context) => {
                 }
             ],
             "resources": [
-                isHospital ?
                 {
                     "type": "Intensivbetten",
                     "max_available": 16,
                     "in_use": 12,
                     "last_update": new Date().toDateString()
-                } : null,
-                isHospital ?
+                },
                 {
                     "type": "Betten",
                     "max_available": 30,
                     "in_use": 20,
                     "last_update": new Date().toDateString()
-                } : null,
-            ].filter(value => !value),
-            "waiting_times": isHospital ? [] : [
-                {
-                    "type": "Hausbesuch",
-                    "waiting_time": "2 Tage",
-                    "last_update": new Date().toDateString()
-                },
+                }
             ],
-        });
-    }
-
-    return {
-        statusCode: 200,
-        body: JSON.stringify(randomData),
+            "waiting_times": [],
+        }),
     };
 };
