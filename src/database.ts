5c4173f1375391b01bc51afd2c985119ba492ef1
Philipp Donabauer <philipp.donabauer@tarienna.eu>
2020-03-22 00:00:26 +0100

    Added real data querys for database

---
+++ src/database.ts
@@ -0,0 +1,192 @@
+import {Connection, ConnectionConfig, createConnection} from 'mysql';
+
+export interface IOpeningHour {
+    day: string,
+    start_time: any,
+    end_time: any
+}
+
+export interface IResource {
+    type: string,
+    max_capacity: number,
+    current_capacity: number,
+    last_update: any
+}
+
+export interface IWaitingTime {
+    type: string,
+    waiting_time: string,
+    last_update: any
+}
+
+export interface IInstitution {
+    id: number,
+    name: string,
+    type: string,
+    street: string,
+    city: string,
+    postcode: string,
+    phone: string,
+    website: string,
+    email: string,
+    latitude: number,
+    longitude: number,
+    comment: string,
+    opening_hours?: IOpeningHour[],
+    resources?: IResource[],
+    waiting_times?: IWaitingTime[]
+}
+
+export class Database {
+    private static instance: Database;
+
+    connection: Connection;
+
+    private constructor(connection: Connection) {
+        this.connection = connection;
+    }
+
+    static async getInstance(): Promise<Database> {
+        if (this.instance != null) {
+            return this.instance;
+        }
+        try {
+            const connection = await this.promisedConnect();
+            return new Database(connection);
+        } catch (e) {
+            throw new Error('Connecting to database failed!');
+        }
+    }
+
+    private static promisedConnect(): Promise<Connection> {
+        return new Promise<Connection>((resolve, reject) => {
+            const connection = createConnection(this.getConfig());
+            connection.connect(err => {
+                if (err) {
+                    reject(err);
+                    return;
+                }
+
+                resolve(connection);
+            })
+        });
+    }
+
+    private static getConfig(): ConnectionConfig {
+        return {
+            host: process.env.RDS_HOSTNAME,
+            user: process.env.RDS_USERNAME,
+            password: process.env.RDS_PASSWORD,
+            port: parseInt(process.env.RDS_PORT, 10),
+            database: process.env.RDS_DATABASE
+        };
+    }
+
+    public getInstitution(institutionId: number): Promise<IInstitution> {
+        return new Promise((resolve, reject) => {
+            this.connection.query('SELECT id, name, type, street, city, postcode, phone as phone_number, website, lat as latitude, lon as longitude FROM institutions WHERE id = ? LIMIT 1', [institutionId], (error, dbRecords) => {
+                if (error) {
+                    reject(error);
+                    return;
+                }
+
+                if (!dbRecords) {
+                    resolve(null);
+                    return;
+                }
+
+                const result: IInstitution = dbRecords[0];
+                const recordId = result.id;
+
+                const promiseOpeningHours = this.queryOpeningHoursForInstitutions(recordId).then(openingHours => {
+                    result.opening_hours = openingHours;
+                    return Promise.resolve(openingHours);
+                });
+
+                const promiseResources = this.queryResourcesForInstitute(recordId).then(resources => {
+                    result.resources = resources;
+                    return Promise.resolve();
+                });
+
+                const promiseWaitingTimes = this.queryWaitingTimes(recordId).then(waitingTimes => {
+                    result.waiting_times = waitingTimes;
+                    return Promise.resolve();
+                });
+
+                Promise.all([promiseOpeningHours, promiseResources, promiseWaitingTimes]).then(() => {
+                    resolve(result);
+                }).catch(reject);
+            });
+        });
+    }
+
+    public queryOpeningHoursForInstitutions(institutionId: number):Promise<IOpeningHour[]> {
+        return new Promise((resolve, reject) => {
+            this.connection.query('SELECT day, start_time, end_time FROM opening_hours WHERE institution_id = ?', [institutionId], (error, dbRecords: any[]) => {
+                if (error) {
+                    reject(error);
+                    return;
+                }
+
+                if (!dbRecords) {
+                    resolve(null);
+                    return;
+                }
+
+                const result: IOpeningHour[] = dbRecords;
+
+                resolve(result);
+            });
+        });
+    }
+
+    public queryResourcesForInstitute(institutionId: number):Promise<IResource[]> {
+        return new Promise((resolve, reject) => {
+            this.connection.query('SELECT resource_type as type, max_capacity, current_capacity, timestamp as last_update FROM resources WHERE institution_id = ?', [institutionId], (error, dbRecords: any[]) => {
+                if (error) {
+                    reject(error);
+                    return;
+                }
+
+                if (!dbRecords) {
+                    resolve(null);
+                    return;
+                }
+
+                const result: IResource[] = dbRecords;
+                resolve(result);
+            });
+        });
+    }
+
+    public queryWaitingTimes(institutionId: number):Promise<IWaitingTime[]> {
+        return new Promise((resolve, reject) => {
+            this.connection.query('SELECT service_type as type, waiting_time, timestamp as last_update FROM waiting_times WHERE institution_id = ?', [institutionId], (error, dbRecords: any[]) => {
+                if (error) {
+                    reject(error);
+                    return;
+                }
+
+                if (!dbRecords) {
+                    resolve(null);
+                    return;
+                }
+
+                const result: IWaitingTime[] = dbRecords;
+                resolve(result);
+            });
+        });
+    }
+
+    public end() {
+        return new Promise((resolve, reject) => {
+            this.connection.end((err => {
+                if (err) {
+                    reject(err);
+                    return;
+                }
+                resolve();
+            }));
+        });
+    }
+}
--- src/find-institutions.ts
+++ src/find-institutions.ts
@@ -8,19 +8,19 @@ export const handler: APIGatewayProxyHandler = async (event, _context) => {
     if (
         !event
         || !event.queryStringParameters
-        || !event.queryStringParameters['lon']
-        || !event.queryStringParameters['lat']
-        || !event.queryStringParameters['area']
+        || !event.queryStringParameters.lon
+        || !event.queryStringParameters.lat
+        || !event.queryStringParameters.area
     ) {
         return {
             statusCode: 400,
-            body: "Required request parameters: lon, lat, area"
+            body: 'Required request parameters: lon, lat, area'
         };
     }
 
-    const longitude: number = parseFloat(event.queryStringParameters['lon']);
-    const latitude: number = parseFloat(event.queryStringParameters['lat']);
-    const area: number = parseFloat(event.queryStringParameters['area']);
+    const longitude: number = parseFloat(event.queryStringParameters.lon);
+    const latitude: number = parseFloat(event.queryStringParameters.lat);
+    const area: number = parseFloat(event.queryStringParameters.area);
 
     const testDataStartPoint = {
         longitude,
@@ -31,53 +31,53 @@ export const handler: APIGatewayProxyHandler = async (event, _context) => {
 
     for (let i = 0; i < RANDOM_DATA_COUNT; i++) {
         const randomPoint = randomLocation.randomCirclePoint(testDataStartPoint, area * 1000);
-        const isHospital = i % 2 == 0;
+        const isHospital = i % 2 === 0;
 
         randomData.push({
-            "id": i,
-            "name": (isHospital ? "Krankenhaus" : "Arztpraxis") + " " + i,
-            "type": isHospital ? "HOSPITAL" : "DOCTOR",
-            "street": "ANYTHING ELSE",
-            "city": "ANY_CITY",
-            "postal_code": "POSTAL_CODE",
-            "phone_number": "123123131",
-            "website": "http://example.com",
-            "longitude": randomPoint.longitude,
-            "latitude": randomPoint.latitude,
-            "comment": "Bitte nicht ohne Termin",
-            "opening_hours":[
+            'id': i,
+            'name': (isHospital ? 'Krankenhaus' : 'Arztpraxis') + ' ' + i,
+            'type': isHospital ? 'HOSPITAL' : 'DOCTOR',
+            'street': 'ANYTHING ELSE',
+            'city': 'ANY_CITY',
+            'postal_code': 'POSTAL_CODE',
+            'phone_number': '123123131',
+            'website': 'http://example.com',
+            'longitude': randomPoint.longitude,
+            'latitude': randomPoint.latitude,
+            'comment': 'Bitte nicht ohne Termin',
+            'opening_hours':[
                 {
-                    "day": "MONTAG",
-                    "start_time": "08:00",
-                    "end_time": "12:00"
+                    'day': 'MONTAG',
+                    'start_time': '08:00',
+                    'end_time': '12:00'
                 },
                 {
-                    "day": "MONTAG",
-                    "start_time": "12:00",
-                    "end_time": "16:00"
+                    'day': 'MONTAG',
+                    'start_time': '12:00',
+                    'end_time': '16:00'
                 }
             ],
-            "resources": [
+            'resources': [
                 isHospital ?
                 {
-                    "type": "Intensivbetten",
-                    "max_available": 16,
-                    "in_use": 12,
-                    "last_update": new Date().toDateString()
+                    'type': 'Intensivbetten',
+                    'max_available': 16,
+                    'in_use': 12,
+                    'last_update': new Date().toDateString()
                 } : null,
                 isHospital ?
                 {
-                    "type": "Betten",
-                    "max_available": 30,
-                    "in_use": 20,
-                    "last_update": new Date().toDateString()
+                    'type': 'Betten',
+                    'max_available': 30,
+                    'in_use': 20,
+                    'last_update': new Date().toDateString()
                 } : null,
             ].filter(value => !value),
-            "waiting_times": isHospital ? [] : [
+            'waiting_times': isHospital ? [] : [
                 {
-                    "type": "Hausbesuch",
-                    "waiting_time": "2 Tage",
-                    "last_update": new Date().toDateString()
+                    'type': 'Hausbesuch',
+                    'waiting_time': '2 Tage',
+                    'last_update': new Date().toDateString()
                 },
             ],
         });
