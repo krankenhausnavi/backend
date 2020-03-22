a1582e210c51e5c66c5aef266f7a99539c25289d
Philipp Donabauer <philipp.donabauer@tarienna.eu>
2020-03-22 14:25:11 +0100

    Implemented get institutions

--- src/database.ts
+++ src/database.ts
@@ -37,6 +37,12 @@ export interface IInstitution {
     waiting_times?: IWaitingTime[]
 }
 
+// @ts-ignore
+interface IContainerForInstitute<O> {
+    item: O,
+    institution_id: number,
+}
+
 export class Database {
     private static instance: Database;
 
@@ -85,97 +91,123 @@ export class Database {
 
     public getInstitution(institutionId: number): Promise<IInstitution> {
         return new Promise((resolve, reject) => {
-            this.connection.query('SELECT id, name, type, street, city, postcode, phone as phone_number, website, lat as latitude, lon as longitude FROM institutions WHERE id = ? LIMIT 1', [institutionId], (error, dbRecords) => {
-                if (error) {
-                    reject(error);
-                    return;
-                }
+            this.connection.query(
+            `SELECT
+                    inst.id,
+                    inst.name,
+                    inst.type,
+                    inst.street,
+                    inst.city,
+                    inst.postcode,
+                    inst.phone                                                    as phone_number,
+                    inst.website,
+                    inst.lat                                                      as latitude,
+                    inst.lon                                                      as longitude,
+                    CONCAT('[',IFNULL(CONVERT(GROUP_CONCAT(DISTINCTROW JSON_OBJECT(
+                            'day', oh.day, 'start_time',
+                            oh.start_time, 'end_time',
+                            oh.end_time) SEPARATOR ',')
+                                              USING
+                                              utf8), ''), ']') as opening_hours,
+                    CONCAT('[',CONVERT(GROUP_CONCAT(DISTINCTROW JSON_OBJECT('type', res.resource_type,
+                                                                            'max_capacity',
+                                                                            res.max_capacity,
+                                                                            'current_capacity',
+                                                                            res.current_capacity,
+                                                                            'last_update', res.timestamp)
+                                                    SEPARATOR ',') USING utf8) , ']') as resources,
+                    CONCAT('[', CONVERT(GROUP_CONCAT(DISTINCTROW JSON_OBJECT('type', wt.service_type,
+                                                                             'waiting_time', wt.waiting_time,
+                                                                             'last_update', wt.timestamp)
+                                                     SEPARATOR ',') USING utf8) , ']') as waiting_times
+            FROM 
+                institutions as inst,
+                opening_hours as oh,
+                resources as res,
+                waiting_times as wt
+            WHERE inst.id = ?
+                AND oh.institution_id = inst.id
+                AND res.institution_id = inst.id
+                AND wt.institution_id = inst.id
+            GROUP BY inst.id`,
+        [institutionId],
+        (error, dbRecords) => {
+                    if (error) {
+                        reject(error);
+                        return;
+                    }
 
-                if (!dbRecords) {
-                    resolve(null);
-                    return;
-                }
+                    if (!dbRecords) {
+                        resolve(null);
+                        return;
+                    }
 
-                const result: IInstitution = dbRecords[0];
-                const recordId = result.id;
-
-                const promiseOpeningHours = this.queryOpeningHoursForInstitutions(recordId).then(openingHours => {
-                    result.opening_hours = openingHours;
-                    return Promise.resolve(openingHours);
-                });
-
-                const promiseResources = this.queryResourcesForInstitute(recordId).then(resources => {
-                    result.resources = resources;
-                    return Promise.resolve();
-                });
-
-                const promiseWaitingTimes = this.queryWaitingTimes(recordId).then(waitingTimes => {
-                    result.waiting_times = waitingTimes;
-                    return Promise.resolve();
-                });
-
-                Promise.all([promiseOpeningHours, promiseResources, promiseWaitingTimes]).then(() => {
+                    const result: IInstitution = dbRecords[0];
                     resolve(result);
-                }).catch(reject);
-            });
+
+                }
+                );
         });
     }
 
-    public queryOpeningHoursForInstitutions(institutionId: number):Promise<IOpeningHour[]> {
+    public getInstitutions(latitude: number, longitude: number, area: number): Promise<IInstitution[]> {
         return new Promise((resolve, reject) => {
-            this.connection.query('SELECT day, start_time, end_time FROM opening_hours WHERE institution_id = ?', [institutionId], (error, dbRecords: any[]) => {
-                if (error) {
-                    reject(error);
-                    return;
+            this.connection.query(
+                `SELECT
+                    inst.id,
+                    inst.name,
+                    inst.type,
+                    inst.street,
+                    inst.city,
+                    inst.postcode,
+                    inst.phone                                                    as phone_number,
+                    inst.website,
+                    inst.lat                                                      as latitude,
+                    inst.lon                                                      as longitude,
+                    CONCAT('[',IFNULL(CONVERT(GROUP_CONCAT(DISTINCTROW JSON_OBJECT(
+                                                                'day', oh.day, 'start_time',
+                                                                  oh.start_time, 'end_time',
+                                                                  oh.end_time) SEPARATOR ',')
+                                         USING
+                                         utf8), ''), ']') as opening_hours,
+                    CONCAT('[',CONVERT(GROUP_CONCAT(DISTINCTROW JSON_OBJECT('type', res.resource_type,
+                                                                  'max_capacity',
+                                                                  res.max_capacity,
+                                                                  'current_capacity',
+                                                                  res.current_capacity,
+                                                                  'last_update', res.timestamp)
+                                                      SEPARATOR ',') USING utf8) , ']') as resources,
+                    CONCAT('[', CONVERT(GROUP_CONCAT(DISTINCTROW JSON_OBJECT('type', wt.service_type,
+                                                                  'waiting_time', wt.waiting_time,
+                                                                  'last_update', wt.timestamp)
+                                                      SEPARATOR ',') USING utf8) , ']') as waiting_times
+            FROM 
+                institutions as inst,
+                opening_hours as oh,
+                resources as res,
+                waiting_times as wt
+            WHERE
+                 ST_DISTANCE(POINT(?, ?), POINT(inst.lat, inst.lon)) < ?
+                AND oh.institution_id = inst.id
+                AND res.institution_id = inst.id
+                AND wt.institution_id = inst.id
+            GROUP BY inst.id`,
+                [latitude, longitude, area],
+                (error, dbRecords) => {
+                    if (error) {
+                        reject(error);
+                        return;
+                    }
+
+                    if (!dbRecords) {
+                        resolve(null);
+                        return;
+                    }
+
+                    const result: IInstitution[] = dbRecords;
+                    resolve(result);
                 }
-
-                if (!dbRecords) {
-                    resolve(null);
-                    return;
-                }
-
-                const result: IOpeningHour[] = dbRecords;
-
-                resolve(result);
-            });
-        });
-    }
-
-    public queryResourcesForInstitute(institutionId: number):Promise<IResource[]> {
-        return new Promise((resolve, reject) => {
-            this.connection.query('SELECT resource_type as type, max_capacity, current_capacity, timestamp as last_update FROM resources WHERE institution_id = ?', [institutionId], (error, dbRecords: any[]) => {
-                if (error) {
-                    reject(error);
-                    return;
-                }
-
-                if (!dbRecords) {
-                    resolve(null);
-                    return;
-                }
-
-                const result: IResource[] = dbRecords;
-                resolve(result);
-            });
-        });
-    }
-
-    public queryWaitingTimes(institutionId: number):Promise<IWaitingTime[]> {
-        return new Promise((resolve, reject) => {
-            this.connection.query('SELECT service_type as type, waiting_time, timestamp as last_update FROM waiting_times WHERE institution_id = ?', [institutionId], (error, dbRecords: any[]) => {
-                if (error) {
-                    reject(error);
-                    return;
-                }
-
-                if (!dbRecords) {
-                    resolve(null);
-                    return;
-                }
-
-                const result: IWaitingTime[] = dbRecords;
-                resolve(result);
-            });
+            );
         });
     }
 
